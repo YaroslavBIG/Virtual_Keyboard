85f9d3d4408f513089977009c0942553405ca072
YaroslavBIG <yaroslavbig@ya.ru>
2020-04-01 02:14:08 +0300

    feet: add functions script.js

--- script.js
+++ script.js
@@ -25,5 +25,48 @@ function addKeybord() {
 
 }
 addKeybord();
+//Add text on input area
+const textArea = document.querySelector(".input_board");
 
-//<textarea class="use-keyboard-input" style="position: absolute; top: 130px; right: 30px; width: 300px;"></textarea>
+function addText(text) {
+    textArea.value += `${text}`;
+}
+//Backspace
+const backspace = () => textArea.value = textArea.value.substring(0, textArea.value.length - 1);
+
+//Clear
+const clearTextArea = () => textArea.value = "";
+
+function createKeyIcons(icon_name) {
+    return `<i class="material-icons">${icon_name}</i>`
+}
+
+function createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+        "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+        "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+        "space"
+    ];
+
+    keyLayout.forEach(key =>{
+        const keyEl = document.createElement("button");
+        const insertLineBreak = ["backspase", "p", "enter", "?"].indexOf(key) === 1;
+
+        keyEl.setAttribute("type", "button");
+        keyEl.classList.add("keyboard__key");
+
+        switch (key) {
+            case "backspace":
+                keyEl.classList.add("keyboard__key--wide");
+                keyEl.innerHTML = createKeyIcons("backspace");
+                keyEl.addEventListener("click", () => {
+
+                });
+                break;
+        }
+    })
+}
+
